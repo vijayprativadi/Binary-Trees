7d6714e7dc83c561a405a332a6b70dd362f02514
Vijay Prativadi <vijayprativadi@hotmail.com>
2020-08-02 15:36:37 -0400

    Added Pre Order, In Order & Post Order Traversal

---
+++ BinaryTrees/BinaryTrees/BinaryTree.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTrees
+{
+    public class Node
+    {
+        public int data { get; set; }
+        public Node left { get; set; }
+        public Node right { get; set; }
+        public Node()
+        {
+
+        }
+        public Node(int data)
+        {
+            this.data = data;
+            left = null;
+            right = null;
+
+        }
+    }
+    public class BinaryTree
+    {
+        public Node root;
+        public BinaryTree()
+        {
+            root = null;
+        }
+
+        //Wrapper Function 
+        public void PreOrderTraversal()
+        {
+            PreOrderTraversal(root);
+        }
+
+        //Center/Left/Right Traversal
+        private void PreOrderTraversal(Node root)
+        {
+            if (root == null) return;
+
+            System.Console.WriteLine(root.data + " ");
+            PreOrderTraversal(root.left);
+            PreOrderTraversal(root.right);
+
+        }
+
+        //Wrapper Function 
+        public void InOrderTraversal()
+        {
+            InOrderTraversal(root);
+        }
+
+        //Left/Center/Right Traversal
+        private void InOrderTraversal(Node root)
+        {
+            if (root == null) return;
+
+            InOrderTraversal(root.left);
+            System.Console.WriteLine(root.data + " ");
+            InOrderTraversal(root.right);
+        }
+
+        //Wrapper Function 
+        public void PostOrderTraversal()
+        {
+            PostOrderTraversal(root);
+        }
+
+        //Left/Right/Center Traversal
+        private void PostOrderTraversal(Node root)
+        {
+            if (root == null) return;
+
+            PostOrderTraversal(root.left);
+            PostOrderTraversal(root.right);
+            System.Console.WriteLine(root.data + " ");
+        }
+    }
+}
--- BinaryTrees/BinaryTrees/Program.cs
+++ BinaryTrees/BinaryTrees/Program.cs
@@ -10,6 +10,65 @@ namespace BinaryTrees
     {
         static void Main(string[] args)
         {
+            BinaryTree binaryTree = new BinaryTree();
+            binaryTree.root = new Node(1);
+            binaryTree.root.left = new Node(2);
+            binaryTree.root.right = new Node(3);
+            binaryTree.root.left.left = new Node(4);
+            binaryTree.root.left.right = new Node(5);
+
+            //Pre Order Traversal
+            Console.WriteLine("Pre Order Traversal");
+            binaryTree.PreOrderTraversal();
+
+            //In Order Traversal
+            Console.WriteLine("In Order Traversal");
+            binaryTree.InOrderTraversal();
+
+            //Post Order Traversal
+            Console.WriteLine("Post Order Traversal");
+            binaryTree.PostOrderTraversal();
+
+            Console.ReadKey();
+            //08/02/2020
+            //Level order traversal
+            //BFS
+            //DFS
+            //Find Parent of a Node
+            //Find Sibling Of a Node
+
+            //08/03/2020
+            //Find Cousin of a Node
+            //Sum of all leaf nodes
+            //Branch Sum
+            //Min Depth or height
+            //Max Depth or height
+            //Boundry Traversal
+            //Check If binary trees are identical
+
+            //08/04/2020
+            //Check if binary tree is part of subtree
+            //Top View
+            //Bottom View
+            //Left View
+            //Right View
+            //Delete Binary Tree
+            //Diameter of Binary Tree
+            //Delete Binary Tree
+
+            //08/05/2020
+            //LCS
+            //Max Path Sum
+            //Reverse Alternate Levels
+            //Invert Binary Tree
+            //Closed Value in BST
+            //Valid Binary Tree
+            //Flatten Binary Tree
+            //Max Path Sum
+
+            //08/06/2020
+            //Node Depth
+            //Mirror Tree
         }
     }
 }
